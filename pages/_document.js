128c8df6737ad1198e36e194cdea9da40dcba8bd
Aman1601k <69238926+Aman1601k@users.noreply.github.com>
2022-02-24 14:06:05 +0530

    deploy bug fixes

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          < meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
