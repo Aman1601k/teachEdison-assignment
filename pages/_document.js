02ed941cef2e41fc1159ed4a3667f576095dfc41
Aman1601k <69238926+Aman1601k@users.noreply.github.com>
2022-02-24 14:08:23 +0530

    minor deploy bug

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          < meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+          < meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
